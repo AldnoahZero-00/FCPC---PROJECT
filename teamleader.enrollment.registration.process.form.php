3195b237b2ad7b768062bab79c37cba721beb521
Artemson <60414364+AldnoahZero-00@users.noreply.github.com>
2022-06-28 16:52:46 +0800

    Add files via upload

--- teamleader.enrollment.registration.process.form.php
+++ teamleader.enrollment.registration.process.form.php
@@ -1,183 +1,219 @@
 <?php
-	include('controller/class/configuration/connection.php');
+    include('controller/class/configuration/connection.php');
 
-	if(isset($_SESSION["userid"]))
-	{
-		$userid = $_SESSION['userid'];
-	} 
-	else 
-	{
-		$userid = 0;
-	}
-	$qryreg = "SELECT `reg`.`schlenrollprereg_regdate` `REG_DATE`,
-					  UPPER(CONCAT(`reg`.`schlenrollprereg_lname`, ', ', `reg`.`schlenrollprereg_fname` , ' ' , `reg`.`schlenrollprereg_mname`)) `NAME`,
-					  UPPER(`lvl`.`schlacadlvl_name`) `LVL_NAME`, 
-					  UPPER(`yrlvl`.`schlacadyrlvl_name`) `YRLVL_NAME`, 
-					  UPPER(`crse`.`schlacadcrse_code`) `CRSE_NAME`, 
-					  `reg`.`schlusr_id` `USER_ID`,
-					  `reg`.`schlenrollprereg_id` `VIEW`,
-					  `reg`.`schlenrollprereg_id` `PROCESS`, 
-					  `lvl`.`schlacadlvl_name` `level` 
-					FROM `school_enrollment_pre_registration` `reg`
-						LEFT JOIN `school_academic_level` `lvl`
-							ON `reg`.`acadlvl_id`=`lvl`.`schlacadlvl_id`
-						LEFT JOIN `school_academic_year_level` `yrlvl`
-							ON `reg`.`acadyrlvl_id`=`yrlvl`.`schlacadyrlvl_id`
-						LEFT JOIN `school_academic_course` `crse`
-							ON `reg`.`acadcrse_id`=`crse`.`schlacadcrse_id`
-						LEFT JOIN `school_users` `usr`
-							ON `reg`.`schlusr_id`=`usr`.`schlusr_id`
-							WHERE `reg`.`schlusr_id` "; 
-
-	$rsreg = $dbConn->query($qryreg);
-	$fetchDatareg = $rsreg->fetch_ALL(MYSQLI_ASSOC);
-	
-	$qryuser = "SELECT `schlusr_lname` `NAME`,
-				       `schlusr_id` `USER_ID`
-				FROM `school_users`
-					WHERE `schlusr_status` = 1 
-					AND `schlusr_isactive` = 1 ";
-	$rsuser = $dbConn->query($qryuser);
-	$fetchDatauser = $rsuser->fetch_ALL(MYSQLI_ASSOC);
+    if(isset($_SESSION["userid"]))
+    {
+        $userid = $_SESSION['userid'];
+    } 
+    else 
+    {
+        $userid = 0;
+    }
+    $qryreg = "SELECT `reg`.`schlenrollprereg_regdate` `REG_DATE`,
+        UPPER(CONCAT(`reg`.`schlenrollprereg_lname`, ', ', `reg`.`schlenrollprereg_fname` , ' ' , `reg`.`schlenrollprereg_mname`)) `NAME`,
+        UPPER(`lvl`.`schlacadlvl_name`) `LVL_NAME`, 
+        UPPER(`yrlvl`.`schlacadyrlvl_name`) `YRLVL_NAME`, 
+        UPPER(`crse`.`schlacadcrse_code`) `CRSE_NAME`,
+            `reg`.`schlenrollprereg_id` `STUD_ID`, 
+            `reg`.`schlusr_id` `USER_ID`,
+            `reg`.`schlenrollprereg_id` `VIEW`,
+            `reg`.`schlenrollprereg_id` `PROCESS`, 
+            `lvl`.`schlacadlvl_name` `level` 
+        FROM `school_enrollment_pre_registration` `reg`
+            LEFT JOIN `school_academic_level` `lvl`
+                ON `reg`.`acadlvl_id`=`lvl`.`schlacadlvl_id`
+            LEFT JOIN `school_academic_year_level` `yrlvl`
+                ON `reg`.`acadyrlvl_id`=`yrlvl`.`schlacadyrlvl_id`
+            LEFT JOIN `school_academic_course` `crse`
+                ON `reg`.`acadcrse_id`=`crse`.`schlacadcrse_id`
+            LEFT JOIN `school_users` `usr`
+                ON `reg`.`schlusr_id`=`usr`.`schlusr_id`"; 
+    $rsreg = $dbConn->query($qryreg);
 
 
-	
+    $qryuser = "SELECT `schlusr_lname` `NAME`,
+                    `schlusr_id` `USER_ID`
+                FROM `school_users`
+                WHERE `schlusr_status` = 1 
+                AND `schlusr_isactive` = 1 ";
+    $rsuser = $dbConn->query($qryuser);
+
+    
 ?>
-	<link href="tool/bootstrap-5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"/>
-	<script src="tool/bootstrap-5.0.2/dist/js/bootstrap.bundle.min.js"></script>
-	<script src="tool/jquery-3.6.0.min.js"></script>
-	<body>
-  	<div class="container">
-  		<br>
+
+
+
+
+    <link href="tool/bootstrap-5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"/>
+    <script src="tool/bootstrap-5.0.2/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="tool/jquery-3.6.0.min.js"></script>
+    <body>
+    <div class="container">
+        <br>
           <h1 align="center">FIRST CITY PROVIDENTIAL COLLEGE</h1>
           <h2 align="center">ONLINE REGISTRATION</h2>
         <br>
-  	</div>
-  	<div class="container">
-  		<div class="row">
-  			<div class="col-md-2">
-  				
-  			<!--
-  				<div class="dropdown">
-					<button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">Display Data</button>
-					<ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-						<li><a class="dropdown-item" href="#">25</a></li>
-						<li><a class="dropdown-item" href="#">50</a></li>
-						<li><a class="dropdown-item" href="#">100</a></li>
-					</ul>
-				</div>
-			-->
-  			</div>
-  			<div class="col-md-2"></div>
-  			<div class="col-md-2"></div>
-  			<div class="col-md-2"></div>
-			<div class="col-md-4">
-			<!--	
-			<form class="d-flex">
-			  <input id="searchtxt" class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-			  <button class="btn btn-outline-success" type="submit">Search</button>
-			</form>
-		-->
-  			</div>
-  		</div>
-  	</div>
-  	<br>
-    <div class="container" id="table-list">
-		<div id="prereg-data"></div>
-	<?php
-		$createTable  = "<table id='regtable' class='table caption-top table-hover'>";
-		$createTable .= "<thead class='table-dark'>";
-		$createTable .= "<tr>";
-		$createTable .= "<th scope='col'>Timestamp</th>";
-		$createTable .= "<th scope='col'>Student Name</th>";
-		$createTable .= "<th scope='col'>Level</th>";
-		$createTable .= "<th scope='col'>Grade Level</th>";
-		$createTable .= "<th scope='col'>Course Strand</th>";
-		$createTable .= "<th scope='col'>Assigned to</th>";
-		$createTable .= "<th scope='col'>Processed</th>";
-		$createTable .= "<th scope='col'>Actions</th>";
-		$createTable .= "</tr>";
-		$createTable .= "</thead>";
-		$createTable .= "<tbody>";
-		$createTable .= "<div></div>";
-		
-		foreach($fetchDatareg as $regitem)
-		{	
-			
-			$createTable .= "<tr>";
-			$createTable .= "<td>".$regitem['REG_DATE']."</td>";
-			$createTable .= "<td>".$regitem['NAME']."</td>";
-			$createTable .= "<td>".$regitem['LVL_NAME']."</td>";
-			$createTable .= "<td>".$regitem['YRLVL_NAME']."</td>";
-			$createTable .= "<td>".$regitem['CRSE_NAME']."</td>";
-			$createTable .= "<td>";
-			$createTable .= "<div class='dropdown'>";
-			$createTable .= "<select id='list' name='list' class='form-control dropdown'>";
-			$createTable .= "<option value='0'>Unassigned</option>";
-			foreach($fetchDatauser as $useritem)
-			{
-					$createTable .= "<option value='".$useritem['USER_ID']."'>".$useritem['NAME']."</option>";
-			}
-			$createTable .= "</select>";
-			$createTable .= "</div>";
-			$createTable .= "<td>";
-			$createTable .= "</td>";
-			$createTable .= "</td>";
-			$createTable .= "<td>";
-			$createTable .= "<div class='btn-group' role='group' aria-label='Basic example'>";
-			$createTable .= "<a href='enrollmentregistrationform.php?userid=".$regitem['VIEW']."' target = '_blank'><button type='button' class='btn btn-primary'>View Profile</button></a>";
-			$createTable .= "<input type='hidden' id='".$regitem['VIEW']."' name='prereg_id' value='".$regitem['VIEW']."'>";
-
-			$createTable .= "<button type='button' class='btn btn-danger' hidden>Process</button>";
-			$createTable .= "<input type='hidden' id='".$regitem['PROCESS']."' name='prereg_id' value='".$regitem['PROCESS']."'>";
-
-			$createTable .= "</div>";
-			$createTable .= "</td>";
-			$createTable .= "</tr>";
-		}
-		$createTable .= "</tbody>";
-		$createTable .= "</table>";
-			
-		echo $createTable;
-		$rsreg->close();
-		//$dbConn->close();
-	?>
-    </div>	
-    <div class="container">
-    	<div class="row">
-    		<div class="col-md-4"></div>
-    		<div class="col-md-4">
-				<nav aria-label="Page navigation example">
-					<ul class="pagination">
-						<li class="page-item"><a class="page-link" href="#">Previous</a></li>
-						<li class="page-item"><a class="page-link" href="#">1</a></li>
-						<li class="page-item"><a class="page-link" href="#">2</a></li>
-						<li class="page-item"><a class="page-link" href="#">3</a></li>
-						<li class="page-item"><a class="page-link" href="#">Next</a></li>
-					</ul>
-				</nav>
-			</div>
-			<div class="col-md-4"></div>
-    	</div>
     </div>
-	<script>
-	$(document).ready(function(){
-		var table = $('#regtable').DataTable();
-		var searchtxt = $('#searchtxt').val();
-		var filteredData = table
-			.columns( 1 )
-			.data()
-			.flatten()
-			.filter( function ( value, index ) {
-				return value = searchtxt ? true : false;
-		});
-		/*$('.list li').click(function(e) {
-			var clicked_element_value = $(this).attr("value");
-			var parent = $(this).parent().attr('name');
-			alert($("#" + parent).val(clicked_element_value));
-		});*/
+    <div class="container">
+        <div class="row">
+            <div class="col-md-2">
+                
+            <!--
+                <div class="dropdown">
+                    <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">Display Data</button>
+                    <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                        <li><a class="dropdown-item" href="#">25</a></li>
+                        <li><a class="dropdown-item" href="#">50</a></li>
+                        <li><a class="dropdown-item" href="#">100</a></li>
+                    </ul>
+                </div>
+            -->
+            </div>
+            <div class="col-md-2"></div>
+            <div class="col-md-2"></div>
+            <div class="col-md-2"></div>
+            <div class="col-md-4">
+            <!--    
+            <form class="d-flex">
+              <input id="searchtxt" class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+              <button class="btn btn-outline-success" type="submit">Search</button>
+            </form>
+        -->
+            </div>
+        </div>
+    </div>
+    <br>
+    <div class="container" id="table-list">
+        <div id="prereg-data"></div>
+            <table id='regtable' class='table caption-top table-hover'>
+                <thead class='table-dark'>
+                    <tr>
+                        <th scope='col'>Timestamp</th>
+                        <th scope='col'>Student Name</th>
+                        <th scope='col'>Level</th>
+                        <th scope='col'>Grade Level</th>
+                        <th scope='col'>Course Strand</th>
+                        <th scope='col'>Assigned to</th>
+                        <th scope='col'>Processed</th>
+                        <th scope='col'>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <div>
 
-	});
+                <?php 
+                    $count=0;
+                    if ($rsreg -> num_rows >  0) 
+                    {
+                        while ($fetchDatareg = $rsreg->fetch_assoc()) 
+                        {
+                            $count=$count+1;
+                ?>
+                    <tr>
+                        <td><?php echo $fetchDatareg["REG_DATE"]        ?></td>
+                        <td><?php echo $fetchDatareg["NAME"]            ?></td>
+                        <td><?php echo $fetchDatareg["LVL_NAME"]        ?></td>
+                        <td><?php echo $fetchDatareg["YRLVL_NAME"]      ?></td>
+                        <td><?php echo $fetchDatareg["CRSE_NAME"]       ?></td>
+                        <td>
+                            <select id='user_list' name='user_list' class='form-control dropdown'>
+                                <?php
+                                    $rsuser = $dbConn->query($qryuser);
+                                    $count=0;
+
+
+                                    echo "<option value='0'>Unassigned</option>";
+                                    if ($rsuser -> num_rows >  0) 
+                                    {
+                                        while ($rsuser_row = $rsuser->fetch_assoc()) 
+                                        {
+                                            $count=$count+1;
+                                ?>  
+                                            <option value='<?php echo $rsuser_row["USER_ID"]?>'><?php echo $rsuser_row["NAME"]?></option>
+                                <?php
+                                        }
+                                    }
+                                    
+                                ?>     
+                            </select>
+                        </td>
+                        <td>
+ 
+                        </td>
+                        <td>
+                            <div class='btn-group' role='group' aria-label='Basic example'>
+                            <a href='enrollmentregistrationform.php?userid="<?php echo $rsuser_row["VIEW"]?>"' target = '_blank'>
+                            	<button type='button' class='btn btn-primary'>View Profile</button></a>
+                            <input type='hidden' id='".$regitem['VIEW']."' name='prereg_id' value='".$regitem['VIEW']."'>
+
+                            <button type='button' class='btn btn-danger' hidden>Process</button>
+                            <input type='hidden' id='".$regitem['PROCESS']."' name='prereg_id' value='".$regitem['PROCESS']."'>
+                            
+                            </div>
+                        </td>
+                    </tr>
+            <?php
+                        }
+                    }
+            ?>
+                    </div>
+                </tbody>
+            </table>
+
+    </div>  
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4"></div>
+            <div class="col-md-4">
+                <nav aria-label="Page navigation example">
+                    <ul class="pagination">
+                        <li class="page-item"><a class="page-link" href="#">Previous</a></li>
+                        <li class="page-item"><a class="page-link" href="#">1</a></li>
+                        <li class="page-item"><a class="page-link" href="#">2</a></li>
+                        <li class="page-item"><a class="page-link" href="#">3</a></li>
+                        <li class="page-item"><a class="page-link" href="#">Next</a></li>
+                    </ul>
+                </nav>
+            </div>
+            <div class="col-md-4"></div>
+        </div>
+    </div>
+
+<script type="text/javascript">
+
+
+    $(document).ready(function(){
+            $("#user_list").change(function(){
+                $("#loader").show();
+                var get_user_list_Id = $(this).val();
+                var get_stud_list_Id = $(<?php echo $fetchDatareg["STUD_ID"];?>).val();
+
+                $.ajax({
+                    type:'POST',
+                    url: 'test(php).php',
+                    data: { user_id:get_user_list_Id,
+                            stud_id:et_stud_list_Id
+                          }
+                });
+            });
+
+
+    $(document).ready(function(){
+        var table = $('#regtable').DataTable();
+        var searchtxt = $('#searchtxt').val();
+        var filteredData = table
+            .columns( 1 )
+            .data()
+            .flatten()
+            .filter( function ( value, index ) {
+                return value = searchtxt ? true : false;
+        });
+        /*$('.list li').click(function(e) {
+            var clicked_element_value = $(this).attr("value");
+            var parent = $(this).parent().attr('name');
+            alert($("#" + parent).val(clicked_element_value));
+        });*/
+
+    });
   </script>
   </body>
- 
